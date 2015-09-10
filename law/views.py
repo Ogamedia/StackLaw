2c9e2639d579370089029f7902dd2e4c33a92ef2
Ogamedia <kwame.yeboah@meltwater.org>
2015-09-10 15:34:16 +0000

    added urls and login views

--- law/views.py
+++ law/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.views import generic
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.contrib import auth
+from django.core.context_processors import csrf
+
 
 # Create your views here.
 def index(request):
     return render(request, 'law/index.html')
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    render_to_response('login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', ' ')
+    password = request.POST.get('password', ' ')
+    user = auth.authenticate(username = username, password = password)
+    
+    if user is not None:
+        auth.login(requests, user)
+        return HttpResponseRedirect('law/index')
+    else:
+        return HttpResponseRedirect('law/registration/auth.html')
--- stacklaw/urls.py
+++ stacklaw/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^law/', include('law.urls', namespace='law'))
+    url(r'^law/', include('law.urls', namespace='law')),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'law/registration/login.html'}),
+    url(r'^accounts/auth/$', auth_views.auth ),
 ]
