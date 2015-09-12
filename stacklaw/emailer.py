78e2ce48a0be5cc3689232559955364862d755de
Ogamedia <kwame.yeboah@meltwater.org>
2015-09-12 17:28:11 +0000

    added emailer settings to file

---
+++ stacklaw/emailer.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kwame.yeboah@meltwater.org'
+EMAIL_HOST_PASSWORD ='Ogamedia1'
+EMAIL_PORT = 587
--- stacklaw/settings.py
+++ stacklaw/settings.py
@@ -10,6 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
+# mail configuration
+from .emailer import *
+
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD =EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
