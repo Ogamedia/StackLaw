613600a29c2816465f3159507c3242bf69bde098
Ogamedia <kwame.yeboah@meltwater.org>
2015-09-13 12:47:15 +0000

    Basic email now sends

--- stacklaw/emailer.py
+++ stacklaw/emailer.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kwame.yeboah@meltwater.org'
-EMAIL_HOST_PASSWORD ='Ogamedia1'
+EMAIL_HOST_PASSWORD ='ogamedia1'
 EMAIL_PORT = 587
