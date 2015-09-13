751adf9b3cbcfb155d66f9169eb6d76c9024b394
Ogamedia <kwame.yeboah@meltwater.org>
2015-09-13 13:07:16 +0000

    remove user_profile file

--- law/models.py
+++ law/models.py
@@ -10,4 +10,17 @@ class ReviewRequest(models.Model):
     
     def __unicode__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    """docstring for UserProfile"""
+    user = models.OneToOneField(User)
+    activation_key = models.Charfield(max_length=40, blank=True)
+    key_expires = models.DateTimeField(default-datetime.date.today())
+    
+    def __unicode__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name_plural = u'User Profiles'
         
